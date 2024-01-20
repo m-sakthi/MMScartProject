974f2b45134b9ebf87faa3f87eee023319f18666
Ragav s <ragav2524@gmail.com>
2024-01-20 21:03:01 +0530

    Message

--- frontend/src/components/layouts/Footer.js
+++ frontend/src/components/layouts/Footer.js
@@ -2,7 +2,7 @@ export  default function Footer (){
     return (
         <footer className="py-1">
             <p className="text-center text-dark mt-1">
-                MMScart - 2022-2023, All Rights Reserved
+                MMScart - 2023-2024, All Rights Reserved
             </p>
         </footer>
     )
